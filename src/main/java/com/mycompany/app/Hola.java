319c5f5c7503bf8c6676f4ac514d9c3cd92a525e
ylakhdar <ylakhdar@coveo.com>
2023-06-06 14:44:37 -0400

    docs: add class doc

--- src/main/java/com/mycompany/app/Hola.java
+++ src/main/java/com/mycompany/app/Hola.java
@@ -2,6 +2,7 @@ package com.mycompany.app;
 
 /**
  * Hello world!
+ * Quisque aliquet, urna ultricies ultrices laoreet, arcu ante interdum tortor, non lacinia turpis justo sed odio. Etiam eu nibh et ex efficitur lacinia euismod a neque. Maecenas commodo lacus risus, in pulvinar turpis malesuada vitae. Aenean efficitur aliquet pulvinar. Fusce placerat placerat nisl et molestie. In pharetra libero sit amet tempor posuere. Vivamus interdum ullamcorper velit, eget bibendum nisl vulputate non.
  *
  */
 public class Hola {
