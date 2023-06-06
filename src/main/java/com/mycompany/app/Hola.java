b6544daefae3520f153927912a6a777a639b89c3
ylakhdar <ylakhdar@coveo.com>
2023-06-06 14:44:05 -0400

    feat: add `run` method

--- src/main/java/com/mycompany/app/Hola.java
+++ src/main/java/com/mycompany/app/Hola.java
@@ -8,4 +8,8 @@ public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola World!");
     }
+
+    public String run(String args) {
+        return "holla" + args;
+    }
 }
